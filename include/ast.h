401b1c4f284e4790be22a13cbc5f473d4e9c640c
garfr <gavratcliff@gmail.com>
2021-05-03 14:09:47 -0500

    feat: basic AST layout

---
+++ include/ast.h
@@ -0,0 +1,41 @@
+#ifndef AST_H
+#define AST_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "helper.h"
+
+enum {
+  BINOP_ADD,
+  BINOP_SUB,
+  BINOP_MUL,
+  BINOP_DIV,
+};
+
+enum {
+  EXPR_INT,
+  EXPR_BINOP,
+};
+
+typedef struct Expr {
+  int t;
+  const uint8_t* start;
+  size_t sz;
+
+  union {
+    struct {
+      struct Expr* left;
+      struct Expr* right;
+      int op;
+    } binop;
+  } data;
+
+} Expr;
+
+typedef struct {
+  MemPool pool; /* used to allocate structures that belong to this AST */
+  Expr* expr;
+} AST;
+
+#endif

e2e0bd24de999907b136c24594dd4f3d6bc57278
garfr <gavratcliff@gmail.com>
2021-05-06 11:26:17 -0500

    fix: better error messages on return type errors

--- src/sem_returns.c
+++ src/sem_returns.c
@@ -1,5 +1,11 @@
 #include "semantics.h"
 
+enum {
+  RETURN_RIGHT,
+  RETURN_NEVER,
+  RETURN_WRONG,
+};
+
 static int check_fn(AST* ast, Function* fn) {
   for (size_t i = 0; i < fn->body.stmts.items; i++) {
     Stmt* stmt = vector_idx(&fn->body.stmts, i);
@@ -9,22 +15,33 @@ static int check_fn(AST* ast, Function* fn) {
         break;
       case STMT_RETURN:
         if (fn->ret_type->t == TYPE_VOID && stmt->data.ret == NULL) {
-          return 1;
+          return RETURN_RIGHT;
         }
-        return coerce_type(BINOP_ASSIGN, &fn->ret_type, &stmt->data.ret->type,
-                           &ast->pool) != NULL;
+        return (coerce_type(BINOP_ASSIGN, &fn->ret_type, &stmt->data.ret->type,
+                            &ast->pool) != NULL)
+                   ? RETURN_RIGHT
+                   : RETURN_WRONG;
       default:
         log_internal_err("invalid stmt type %d", stmt->t);
     }
   }
-  return fn->ret_type->t == TYPE_VOID;
+  return (fn->ret_type->t == TYPE_VOID) ? RETURN_RIGHT : RETURN_NEVER;
 }
 
 void check_returns(AST* ast) {
   for (size_t i = 0; i < ast->fns.items; i++) {
     Function* fn = vector_idx(&ast->fns, i);
-    if (!check_fn(ast, fn)) {
-      log_source_err("non-void function never returns", ast->src_base, fn->pos);
+    switch (check_fn(ast, fn)) {
+      case RETURN_RIGHT:
+        break;
+      case RETURN_NEVER:
+        log_source_err("non-void function never returns", ast->src_base,
+                       fn->pos);
+        break;
+      case RETURN_WRONG:
+        log_source_err("function returns incorrect type", ast->src_base,
+                       fn->pos);
+        break;
     }
   }
 }

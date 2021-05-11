f3a38113377c8ee62cda025dcebba6f85556cdd6
garfr <gavratcliff@gmail.com>
2021-05-11 16:37:44 -0500

    feat: proper name resolution for function call subexpressions

--- src/sem_names.c
+++ src/sem_names.c
@@ -1,31 +1,38 @@
 #include "semantics.h"
 
-static void resolve_expr(AST* ast, Scope* scope, Expr* expr) {
+static void
+resolve_expr(AST *ast, Scope *scope, Expr *expr) {
   switch (expr->t) {
     case EXPR_BINOP:
       resolve_expr(ast, scope, expr->data.binop.left);
       resolve_expr(ast, scope, expr->data.binop.right);
       break;
-    case EXPR_VAR: {
-      ScopeEntry* entry = scope_find(scope, expr->pos);
-      if (entry == NULL) {
-        log_source_err("cannot find variable '%.*s'", ast->src_base, expr->pos);
+    case EXPR_VAR:
+      {
+        ScopeEntry *entry = scope_find(scope, expr->pos);
+        if (entry == NULL) {
+          log_source_err("cannot find variable '%.*s'", ast->src_base,
+                         expr->pos);
+        }
+        expr->data.var = entry;
       }
-      expr->data.var = entry;
-    } break;
-    case EXPR_FUNCALL: {
-      ScopeEntry* entry = scope_find(scope, expr->data.funcall.name);
-      if (entry == NULL) {
-        log_source_err("cannot find function '%.*s'", ast->src_base,
-                       expr->data.funcall.name, (int)expr->data.funcall.name.sz,
-                       (char*)expr->data.funcall.name.start);
+      break;
+    case EXPR_FUNCALL:
+      {
+        ScopeEntry *entry = scope_find(scope, expr->data.funcall.name);
+        if (entry == NULL) {
+          log_source_err("cannot find function '%.*s'", ast->src_base,
+                         expr->data.funcall.name,
+                         (int)expr->data.funcall.name.sz,
+                         (char *)expr->data.funcall.name.start);
+        }
+        for (size_t i = 0; i < expr->data.funcall.args.items; i++) {
+          Expr *temp_expr = *((Expr **)vector_idx(&expr->data.funcall.args, i));
+          resolve_expr(ast, scope, temp_expr);
+        }
+        expr->data.funcall.fun = entry;
       }
-      for (size_t i = 0; i < expr->data.funcall.args.items; i++) {
-        Expr* temp_expr = *((Expr**)vector_idx(&expr->data.funcall.args, i));
-        resolve_expr(ast, scope, temp_expr);
-      }
-      expr->data.funcall.fun = entry;
-    } break;
+      break;
     case EXPR_INT:
       break;
     default:
@@ -33,23 +40,25 @@ static void resolve_expr(AST* ast, Scope* scope, Expr* expr) {
   }
 }
 
-void resolve_stmt(AST* ast, Stmt* stmt, Scope* scope) {
+void
+resolve_stmt(AST *ast, Stmt *stmt, Scope *scope) {
   switch (stmt->t) {
-    case STMT_LET: {
-      ScopeEntry* entry =
-          scope_insert(&ast->pool, scope, stmt->data.let.name,
-                       make_var_info(stmt->data.let.mut, stmt->data.let.type));
-      if (entry == NULL) {
-        log_source_err("cannot redeclare variable '%.*s'", ast->src_base,
-                       stmt->pos, (int)stmt->data.let.name.sz,
-                       (char*)stmt->data.let.name.start);
+    case STMT_LET:
+      {
+        ScopeEntry *entry = scope_insert(
+            &ast->pool, scope, stmt->data.let.name,
+            make_var_info(stmt->data.let.mut, stmt->data.let.type));
+        if (entry == NULL) {
+          log_source_err("cannot redeclare variable '%.*s'", ast->src_base,
+                         stmt->pos, (int)stmt->data.let.name.sz,
+                         (char *)stmt->data.let.name.start);
+        }
+        stmt->data.let.var = entry;
+        if (stmt->data.let.value) {
+          resolve_expr(ast, scope, stmt->data.let.value);
+        }
+        break;
       }
-      stmt->data.let.var = entry;
-      if (stmt->data.let.value) {
-        resolve_expr(ast, scope, stmt->data.let.value);
-      }
-      break;
-    }
     case STMT_EXPR:
       resolve_expr(ast, scope, stmt->data.expr);
       break;
@@ -62,10 +71,11 @@ void resolve_stmt(AST* ast, Stmt* stmt, Scope* scope) {
       log_internal_err("invalid stmt type %d", stmt->t);
   }
 }
-void resolve_fn(AST* ast, Function* fn) {
+void
+resolve_fn(AST *ast, Function *fn) {
   fn->scope = scope_init(&ast->pool, ast->global);
   for (size_t i = 0; i < fn->params.items; i++) {
-    Param* param = vector_idx(&fn->params, i);
+    Param *param = vector_idx(&fn->params, i);
     param->entry = scope_insert(&ast->pool, fn->scope, param->name,
                                 make_var_info(0, param->type));
   }
@@ -74,15 +84,16 @@ void resolve_fn(AST* ast, Function* fn) {
   }
 }
 
-void resolve_names(AST* ast) {
+void
+resolve_names(AST *ast) {
   ast->global = scope_init(&ast->pool, NULL);
   for (size_t i = 0; i < ast->fns.items; i++) {
-    Function* fn = vector_idx(&ast->fns, i);
+    Function *fn = vector_idx(&ast->fns, i);
     fn->entry =
         scope_insert(&ast->pool, ast->global, fn->name, make_var_info(0, NULL));
     if (!fn->entry) {
       log_source_err("cannot redeclare function '%.*s'", ast->src_base, fn->pos,
-                     (int)fn->name.sz, (char*)fn->name.start);
+                     (int)fn->name.sz, (char *)fn->name.start);
     }
   }
   for (size_t i = 0; i < ast->fns.items; i++) {
